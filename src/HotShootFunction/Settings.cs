8634c043fed81e86283e0d7ecdb1bcf81782424c
Michał Chęciński <michal.checinski@outlook.com>
2018-02-18 12:44:11 +0100

    Added Settings class to parse app settings... it contains method to split several products divided by semicolon

---
+++ src/HotShootFunction/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotShootFunction
+{
+    public class Settings
+    {
+        public static IEnumerable<string> GetProductsList()
+        {
+            string productsString = Environment.GetEnvironmentVariable("ProductName");
+
+            if (productsString == null)
+            {
+                throw new NullReferenceException("Products list is null");
+            }
+
+            string[] productsArray = productsString.Split(';');
+
+            List<string> productsList = productsArray.ToList();
+            return productsList;
+        }
+    }
+}
